55d11c25dd30e461afa18457f6601a172b1e5f6e
Tigran <tik.sargsyan105@gmail.com>
2022-03-15 10:40:16 +0400

    For change double click in Users

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import {useQuery} from "@apollo/client";
 import {GET_ONE_USERS} from "./query/user";
 import CreateUser from "./components/CreateUser";
 import Users from "./components/Users";
+
 function App() {
     const {
         data: oneUser,
@@ -14,13 +15,13 @@ function App() {
     console.log(oneUser, "+++++++++++++++++++++++++++++");
 
     return (
-<>
-        <h3 style={{position: "absolute", left: "50px"}}>for change double click in user</h3>
-    <div style={{maxWidth: "70vw", position: "relative"}}>
-        <CreateUser/>
-        <Users/>
-    </div>
-</>
+        <>
+            <h3 style={{position: "absolute", left: "50px"}}>for change double click in user</h3>
+            <div style={{maxWidth: "70vw", position: "relative"}}>
+                <CreateUser/>
+                <Users/>
+            </div>
+        </>
     );
 }
 
